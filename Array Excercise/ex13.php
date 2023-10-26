aed4740c1de54d5bfdb34fbaa40de0d1b48deb59
nandiniimehta <143238234+nandiniimehta@users.noreply.github.com>
2023-10-26 22:23:35 +0530

    Delete Array Excercise directory

--- Array Excercise/ex13.php
+++
@@ -1,6 +0,0 @@
-13. Access and display the person's age from the associative array created in question 12.
-<?php
-$info=array("name"=>'Nandini',"age"=>21,"email"=>'nandinii@gmail.com');
-echo"age=";
-print_r($info['age']);
-?>
