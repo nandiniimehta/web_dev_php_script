30d01372e744a95fd4a8d8fdf857092d7245e1c0
nandiniimehta <143238234+nandiniimehta@users.noreply.github.com>
2023-10-26 21:42:50 +0530

    Add files via upload

---
+++ Array Excercise/ex12.php
@@ -0,0 +1,10 @@
+<?php
+
+/*12. Create an associative array representing a person's details, including name, age, and email.*/
+
+$info=array("name"=>'Nandini',"age"=>21,"email"=>'nandinii@gmail.com');
+print_r($info);
+
+/*output:
+    Array ( [name] => Nandini [age] => 21 [email] => nandinii@gmail.com )*/
+?>
---
+++ Array Excercise/ex13.php
@@ -0,0 +1,6 @@
+13. Access and display the person's age from the associative array created in question 12.
+<?php
+$info=array("name"=>'Nandini',"age"=>21,"email"=>'nandinii@gmail.com');
+echo"age=";
+print_r($info['age']);
+?>
